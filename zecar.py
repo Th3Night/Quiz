c99ca1e9f6d3124d4437e1bcfb0fe3090313429a
Slindess <66128725+Slindess@users.noreply.github.com>
2021-09-26 19:40:21 +0300

    Create zecar.py

---
+++ zecar.py
@@ -0,0 +1,22 @@
+file = open("encrypted_string.txt")
+a = file.read()
+file.close()
+
+print(a)
+
+
+alph = "а, б, в, г, д, е, ё, ж, з, и, й, к, л, м, н, о, п, р, с, т, у, ф, х, ц, ч, ш, щ, ъ, ы, ь, э, ю, я, а, б, в, г, д, е, ё, ж, з, и, й, к, л, м, н, о, п, р, с, т, у, ф, х, ц, ч, ш, щ, ъ, ы, ь, э, ю, я"
+alph = alph.split(", ")
+file.close()
+
+print(alph)
+
+for i in range(30):
+    copy = []
+    for l in range(len(a)):
+        copy.append("")
+
+    for j in range(len(copy)):
+            copy[j] = alph[alph.index(a[j]) + i]
+
+    print("".join(copy))
